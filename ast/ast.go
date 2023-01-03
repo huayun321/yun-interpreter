c36b79065299d7565408e7c923cf37260999880e
yun <huayun321@gmail.com>
2023-01-03 10:37:25 +0800

    parse return statement

--- ast/ast.go
+++ ast/ast.go
@@ -28,21 +28,27 @@ func (p Program) TokenLiteral() string {
 	}
 }
 
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
 type LetStatement struct {
 	Token token.Token //token.Let
 	Name  *Identifier
 	value Expression
 }
 
-func (ls *LetStatement) statementNode() {}
-
+func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
-type Identifier struct {
-	Token token.Token // token.IDENT
-	Value string
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
 }
 
-func (i *Identifier) expressionNode() {}
-
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
